52383d66f19eb5b31efbb932a53183b83c91050e
Philippe Batigne <pbatigne@gmail.com>
2020-02-28 20:08:29 -0500

    a lot of clean up

--- app/channels/kiosk_channel.rb
+++ app/channels/kiosk_channel.rb
@@ -1,22 +1,16 @@
 class KioskChannel < ApplicationCable::Channel
   def subscribed
     stream_for Kiosk.find(params[:room])
-    puts "and now I'm streaming"
-    #stream_from "kiosk_channel"
-  #  playing(Kiosk.first )
-    # current_user.appear
   end
   
   def unsubscribed
-    # current_user.disappear
   end
   
-  def playing(data)
-    # current_user.appear(on: data['appearing_on'])
-    puts "IT'S PLAYING #{data}"
+  def play(data)
+    KioskChannel.broadcast_to Kiosk.first, data
   end
 
   def paused(data)
-    puts "IT'S PAUSED #{data}"
+    KioskChannel.broadcast_to Kiosk.first, data
   end
 end
--- app/javascript/channels/kiosk_channel.js
+++ app/javascript/channels/kiosk_channel.js
@@ -1,10 +1,58 @@
 import consumer from "./consumer"
 
+// Select the node that will be observed for mutations
+// const targetNode = document.getElementById('some-id');
+
+// // Options for the observer (which mutations to observe)
+// const config = { attributes: true, childList: true, subtree: true };
+
+// // Callback function to execute when mutations are observed
+// const callback = function(mutationsList, observer) {
+//     // Use traditional 'for loops' for IE 11
+//     for(let mutation of mutationsList) {
+//         if (mutation.type === 'childList') {
+//             console.log('A child node has been added or removed.');
+//         }
+//         else if (mutation.type === 'attributes') {
+//             console.log('The ' + mutation.attributeName + ' attribute was modified.');
+//         }
+//     }
+// };
+
+// // Create an observer instance linked to the callback function
+// const observer = new MutationObserver(callback);
+
+// // Start observing the target node for configured mutations
+// observer.observe(targetNode, config);
+
 consumer.subscriptions.create(
   { channel: "KioskChannel", room: 1 }, {
+
+  player: undefined,
+  button: undefined,
+  fetchPlayer() { return document.getElementById('main-player') },
+  fetchButton() { return document.getElementById('kiosk-1') },
+
   received(data) {
-    debugger
-    console.log("got data", data)
+    console.log(data)
+    if (this.player != null) {
+      console.log('player action')
+      if (data.action == "paused") {
+        console.log("player pausing")
+        this.player.pause();
+      } else if (data.action == "play") {
+        console.log("player playing")
+        this.player.play();
+      }
+    } else if (this.button != null) {
+      if (data.action == "paused") {
+        console.log('here')
+        this.button.innerText = "Play"
+      } else if (data.action == "play") {
+        console.log('here2')
+        this.button.innerText = "Pause"
+      }
+    }
   },
 
   // Called once when the subscription is created.
@@ -15,6 +63,9 @@ consumer.subscriptions.create(
   // Called when the subscription is ready for use on the server.
   connected() {
     setTimeout(function () {
+      this.player = this.fetchPlayer();
+      this.button = this.fetchButton();
+
       this.install()
       this.update()
     }.bind(this), 1000)
@@ -32,68 +83,44 @@ consumer.subscriptions.create(
   },
 
   update() {
-    const player = document.getElementById('main-player');
-    if (player != null) {
-      !player.paused ? this.play() : this.pause()
+    if (this.player != null) {
+      !this.player.paused ? this.play() : this.pause()
     }
   },
 
   play() {
-    // Calls `AppearanceChannel#appear(data)` on the server.
-    // this.perform("appear", { appearing_on: this.appearingOn })
-    console.log("playing!!!")
     this.perform("playing", { kiosk: 1 })
-    if (document.getElementById('kiosk-1') != null)
-      document.getElementById('kiosk-1').innerText = "Pause"
+    if (this.button != null)
+      this.button.innerText = "Pause"
   },
 
   pause() {
-    // Calls `AppearanceChannel#away` on the server.
-    // this.perform("away")
-    console.log("pausing!!!")
     this.perform("paused", { kiosk: 1 })
-    if (document.getElementById('kiosk-1') != null)
-      document.getElementById('kiosk-1').innerText = "Play"
+    if (this.button != null)
+      this.button.innerText = "Play"
   },
 
   install() {
-    console.log("installing...")
-    const player = document.getElementById('main-player');
-
-
-    // player.addEventListener("playing", this.update)
-    // player.addEventListener("pause", this.update)
-    if (player != null) {
-      player.onpause = this.update
-      player.onplay = this.update
-    } else {
-      document.getElementById('kiosk-1').onclick = function () {
-        if (document.getElementById('kiosk-1').innerText === "Play") {
-          this.perform("paused", { kiosk: 1, source: 'button' })
-        } else {
+    if (this.player != null) {
+      this.player.onpause = this.update
+      this.player.onplay = this.update
+    } else if (this.button != null) {
+      this.button.onclick = function (e) {
+        if (e.target.innerText === "Play") {
           this.perform("play", { kiosk: 1, source: 'button' })
-
+        } else {
+          this.perform("paused", { kiosk: 1, source: 'button' })
         }
       }.bind(this)
     }
-
   },
 
   uninstall() {
-    console.log("uninstalling...")
-    const player = document.getElementById('main-player');
-    player.removeEventListener("playing", this.update)
-    player.removeEventListener("pause", this.update)
+    this.player.removeEventListener("playing", this.update)
+    this.player.removeEventListener("pause", this.update)
   },
 
   get isVideoPlaying() {
-    // console.log(this.player)
-    const player = document.getElementById('main-player');
-    console.log("isplaying", player ? !player.paused : false)
-    return player ? !player.paused : false;
+    return this.player ? !this.player.paused : false;
   },
-
-  get player() {
-    return document.getElementById('main-player');
-  }
 })
