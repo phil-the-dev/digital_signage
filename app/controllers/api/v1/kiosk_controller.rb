93da56c2a2e782f1e896e4ddf2382feb2777053d
Philippe Batigne <philippe.batigne@leafly.com>
2020-08-24 15:47:07 -0400

    adding endpoint to get kiosk code

--- app/controllers/api/v1/kiosk_controller.rb
+++ app/controllers/api/v1/kiosk_controller.rb
@@ -1,6 +1,13 @@
+# frozen_string_literal: true
+
 module Api
   module V1
     class KioskController < ApiController
+      def new
+        kiosk = Kiosk.create!
+        render json: { kiosk: { code: kiosk.code, id: kiosk.id } }
+      end
+      
       def videos
         kiosk = Kiosk.find(params[:id])
         videos = kiosk.playable.resolve
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :kiosk, only: [] do
+      resources :kiosk, only: [:new] do
         member do
           get 'videos'
         end
